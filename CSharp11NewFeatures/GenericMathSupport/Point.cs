594c9983267db450f59c8487984066e34ca0a4a2
Jalal Uddin <jalal.exe@gmail.com>
2022-12-21 19:17:14 +0600

    added more codes

---
+++ CSharp11NewFeatures/GenericMathSupport/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMathSupport
+{
+    public class Point<T> where T: INumber<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public Point<T> Sum(Point<T> point)
+        {
+            return new Point<T>() { X = this.X + point.X, 
+                Y = this.Y + point.Y };
+        }
+    }
+}
---
+++ CSharp11NewFeatures/GenericMathSupport/Program.cs
@@ -0,0 +1,9 @@
+﻿using GenericMathSupport;
+
+Point<double> p1 = new Point<double>() { X = 5, Y = 9 };
+Point<double> p2 = new Point<double>() { X = 5, Y = 9 };
+
+var p3 = p1.Sum(p2);
+Console.WriteLine($"{p3.X}, {p3.Y}");
+
+
